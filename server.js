eca1563fa6a342972892dcfc596157e027b20fcf
John Hur <hur.john628@gmail.com>
2021-12-10 04:21:59 -0600

    still figuring out post

--- server.js
+++ server.js
@@ -21,27 +21,23 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile("db/db.json", (data) => {
+        res.json(JSON.parse(data));
+    })
+})
+
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received`);
-    const { title, text } = req.body;
-    if (title && text) {
-        const newNote = {
-            text,
-            title,
-            id: uuid()
-        };
-
-        const noteString = JSON.stringify(newNote);
-        fs.writeFile(`db/db.json`, noteString, 
-        (err, data) => err ? console.log(err) : console.log(`note for ${newNote.title} has been written to json file`));
-
-        const response = {
-            status: "success",
-            body: newNote
-        };
-
-        res.status(201).json(response)
-    } else {res.status(500).json(`Error in posting note!`)}
+    fs.readFile('db/db.json', (data) =>{
+        let jsonDB = JSON.parse(data);
+        let newNote = req.body;
+        newNote.id = jsonDB.length;
+        jsonDB.push(newNote);
+        jsonDB = JSON.stringify(jsonDB);
+        fs.writeFile('db/db.json', jsonDB, (jsonDB) => {
+            res.json(jsonDB);
+        })
+    })
 });
 
 // logs a link to the app
