b3b6929f23b97a61af115453b9c24e7c4b1e17d7
John Hur <hur.john628@gmail.com>
2021-12-09 21:36:52 -0600

    worked on posting data

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const uuid = require('./helpers/uuid')
 
 const PORT = process.env.PORT || 3001;
 
@@ -16,11 +17,34 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/notes',(req, res) =>
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received`);
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            text,
+            title,
+            id: uuid()
+        };
+
+        const noteString = JSON.stringify(newNote);
+        fs.writeFile(`db/db.json`, noteString, 
+        (err, data) => err ? console.log(err) : console.log(`note for ${newNote.title} has been written to json file`));
+
+        const response = {
+            status: "success",
+            body: newNote
+        };
+
+        res.status(201).json(response)
+    } else {res.status(500).json(`Error in posting note!`)}
+});
 
 // logs a link to the app
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
